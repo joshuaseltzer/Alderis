d001b79f99ba4b65850e582dfcb8c74c36cd8d40
Joshua Seltzer <joshua.seltzer90@gmail.com>
2022-02-18 07:07:13 -0500

    Update Assets.swift

--- Alderis/Assets.swift
+++ Alderis/Assets.swift
@@ -52,9 +52,11 @@ internal struct Assets {
 		let font = UIFont.monospacedDigitSystemFont(ofSize: size, weight: .regular)
 		let fontDescriptor = font.fontDescriptor.addingAttributes([
 			.featureSettings: [
-					UIFontDescriptor.FeatureKey.selector: kStylisticAltSixOnSelector,
-					UIFontDescriptor.FeatureKey.type: kStylisticAlternativesType
-			]
+				[
+					.featureIdentifier: kStylisticAlternativesType,
+					.typeIdentifier: kStylisticAltSixOnSelector
+				]
+			] as [[UIFontDescriptor.FeatureKey: Int]]
 		])
 		return UIFont(descriptor: fontDescriptor, size: 0)
 	}
